d7517fe30d4317dfd10dada6b03a558e6f731c20
Honghao Huang (1000090458) <HUANH8@student.op.ac.nz>
2020-10-20 16:36:54 +1300

    added my room and added pubilc key

--- Game Project/Game Project/Program.cs
+++ Game Project/Game Project/Program.cs
@@ -3,16 +3,30 @@ using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace Game_Project
+
 {
+    public struct Item  //add your items
+    {
+        public string keys;
+
+    }
+
+
     class Program
     {
         // Evan's Area
-        
+        public void stuff(Item[] items)
+        {
+            
+            string goldKey;
+            items[0].keys = "goldKey";
+            
+        }
 
 
 
 
-                static void Main(string[] args)
+        static void Main(string[] args)
         {
             EvanRoom();
             Console.ReadLine();
@@ -24,64 +38,112 @@ namespace Game_Project
             Thread.Sleep(500);
             Console.WriteLine("There is a computer scream turns on. It seems that there are some questions need to be answer...");
             Thread.Sleep(500);
-            Console.WriteLine("There are 4 doors(methods) in front of you");
+            Console.WriteLine("Question:If your mother and your girlfriend(boyfriend) fall into a pond and nobody can siwm including yourself");
             Thread.Sleep(500);
-            Console.WriteLine("Which one do you want to get into?");
+            Console.WriteLine("How would you like to do?");
             Thread.Sleep(500);           
             Menu1();
-
-            Console.Beep(); Console.Beep((int)523.2, 300); Console.Beep((int)523.2, 300); Console.Beep((int)523.2, 300);
+        
         }
 
             public static void Menu1()
         {
 
             int selection;
-            string[] title = { "method1", "method2", "method3", "Exit" };
+            string[] title = { 
+                "Pretend not to see. Go home immediately and invite your friends to make a super big party",
+                "Sing a song to make them relax and light a cigarette",
+                "Jumping into the pond Join them", 
+                "It's not my business." };
             do
             {
-                Console.Clear();
-                Console.WriteLine("Main Menu".PadLeft(20));
-                Console.WriteLine("Please Select a number".PadLeft(28));
+
                 for (int i = 0; i < title.Length; i++)
                 {
-                    Console.Write((i + 1).ToString().PadRight(10));
+                    Console.WriteLine();
+                    Console.Write((i + 1).ToString().PadLeft(5).PadRight(10));
                     Console.WriteLine(title[i]);
+                    Console.WriteLine();
                 }
-                // selection to call out task or exit the program
                 selection = Convert.ToInt32(Console.ReadLine());
 
                 switch (selection)
                 {
                     case 1:
-                        method1();
+                        answer1();
                         break;
 
                     case 2:
-                        method2();
+                        answer2();
                         break;
 
                     case 3:
-                        method3();
+                        answer3();
                         break;
+                
                     default:
                         break;
                 }
             } while (selection != 4);
         }
      
-             public static void method1()
-            {
-                Console.WriteLine("Press any key to continue");
-                Console.ReadLine();
-                }
+                     public static void answer1()
+                    {
+
+                    Console.Clear();
+                    Console.WriteLine("What's that noises");
+                    
+                    Console.ReadLine();
+                    Console.WriteLine("The cage is open");
+                    
+                    Console.ReadLine();
+                    Console.WriteLine("Oh my god!! ZOMBIES IS COMING");
+                    
+                    Console.ReadLine();
+                    Console.WriteLine("RUN!!!");
+                    int youDistanceToExit, zombiesDistanceToExit;
+                    Random rand = new Random();
+
+                    youDistanceToExit = rand.Next(10, 20);
+                    zombiesDistanceToExit = youDistanceToExit + rand.Next(15, 25);
+                    
+                    Console.ReadLine();
+
+                    while ((youDistanceToExit < zombiesDistanceToExit) && (youDistanceToExit > 0))
+                    {
+                        
+                        youDistanceToExit = youDistanceToExit - rand.Next(1,3);
+                        zombiesDistanceToExit = zombiesDistanceToExit - rand.Next(2,6);
+
+                        Console.WriteLine($"You are {youDistanceToExit} m from Exit.");
+                        Console.WriteLine($"Zombies are {zombiesDistanceToExit - youDistanceToExit} m from you.");
+                        Console.WriteLine();
+                        Thread.Sleep(500);
+                       
+                        Console.ReadLine();
+
+            }
+
+                    if (zombiesDistanceToExit - youDistanceToExit <= 0)
+                    {
+                        Console.WriteLine("The Zombies ate you....");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You survived....");
+                        Console.ReadLine();
+                    }
+                    Console.ReadLine();
+        }
         
-            public static void method2()
+            public static void answer2()
             {
-   
-            
             
             Console.Clear();
+            Console.WriteLine("What is that sound ? ");
+            Thread.Sleep(500);
+            Console.WriteLine("Oh! My god!! That is a bomb!!!");
             Console.Beep((int)523.2, 300);
             Console.Beep();          
             Console.Beep();
@@ -92,20 +154,33 @@ namespace Game_Project
             Console.Beep();
             Console.Beep();
             Console.Beep();
-            Console.WriteLine("Boommmmmmmmmmmmm!!!!!!!!");
+            Console.Clear();
             Thread.Sleep(500);
+            Console.WriteLine("BOMB!!!!");
+            Thread.Sleep(1000);
             Console.WriteLine("You dead!!");
-
-
+            Console.ReadLine();
         }
         
-            static void method3()
+            static void answer3()
             {
                 Console.Clear();
-                Console.WriteLine("empty for now");
-
-                Console.WriteLine("Press any key to continue");
-                Console.ReadLine();
+                Console.WriteLine("On the floor there is a gold key.");
+                Console.WriteLine("What do you wish to do?");
+                Console.WriteLine("Pick up the key?(yes/no)");
+                string temp = Console.ReadLine();
+            if (temp == "yes")
+            {
+                Console.WriteLine("You walk over to the key and pick it up.");
+                Console.WriteLine("You got a gold key");
+                
+            }
+            else
+            {
+                Console.WriteLine("Good Luck");
+                
+            }
+            Console.ReadLine();
             }
 
 
