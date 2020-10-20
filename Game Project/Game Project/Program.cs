a596c914b1dec02b77f6ffdd7b49fbde6c59d2d3
Alexander Stevenson (1000024146) <STEVAJ3@student.op.ac.nz>
2020-10-20 13:32:22 +1300

    Created the game folder

---
+++ Game Project/Game Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is our game project");
+        }
+    }
+}
