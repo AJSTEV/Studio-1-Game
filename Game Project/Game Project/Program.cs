09def4b374b1e830937f417d38dd22f339775bf5
STEVTN1 <STEVTN1@D201-71867>
2021-03-11 11:03:27 +1300

    Added a capital letter and full stop to line 592.

--- Game Project/Game Project/Program.cs
+++ Game Project/Game Project/Program.cs
@@ -589,7 +589,7 @@ namespace Game_Project
                 {
 
                     Console.WriteLine("You find yourself in a long corridor, it is warm and comfortable.");
-                    Console.WriteLine("something about this room makes you want to sit down and relax");
+                    Console.WriteLine("Something about this room makes you want to sit down and relax.");
                     Console.WriteLine("At the end of this comfortable corridoor is a small green key hanging from what looks like a string");
                     Console.WriteLine("What do you do?");
                     Console.WriteLine("1.Go back through the door you entered.");
