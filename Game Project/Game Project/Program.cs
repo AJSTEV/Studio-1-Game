b79da99c58733bdac9691734b73eaf07d2856f41
STEVTN1 <STEVTN1@D201-71867>
2021-03-11 11:05:17 +1300

    Added a space to line 596.

--- Game Project/Game Project/Program.cs
+++ Game Project/Game Project/Program.cs
@@ -593,7 +593,7 @@ namespace Game_Project
                     Console.WriteLine("At the end of this comfortable corridoor is a small green key hanging from what looks like a string.");
                     Console.WriteLine("What do you do?");
                     Console.WriteLine("1. Go back through the door you entered.");
-                    Console.WriteLine("2.Sit down and relax in comfort.");
+                    Console.WriteLine("2. Sit down and relax in comfort.");
                     Console.WriteLine("3.Run to grab the key.");
 
                     choice = Convert.ToInt32(Console.ReadLine());
